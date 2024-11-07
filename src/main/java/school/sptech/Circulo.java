e1309752bb0efd6ba4f3391d0c5645ed7cd6eddb
Murillo-lc <murillo.marques@sptech.school>
2024-11-07 18:45:45 -0300

    finalizado e testado

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,33 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+
+    private Double raio;
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    public Circulo(){}
+    public Circulo(String cor, Integer espessura, Double raio){
+        super(cor, espessura);
+        this.raio = raio;
+    }
+
+
+    @Override
+    public Double calcularArea() {
+        return raio*raio*Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                '}';
+    }
 }
